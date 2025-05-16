0210f373ba02d30feffd95ec327b50726c2c0dda
Gabriel Lemos <monkzzyprog@gmail.com>
2025-05-16 10:33:41 -0300

    Hello world

--- src/GitExemplo.java
+++ src/GitExemplo.java
@@ -1,12 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+package gitexemplo;
 
-/**
- *
- * @author gabri
- */
-public class GitExemplo {
-    
+public classGitExemplo {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
+              
